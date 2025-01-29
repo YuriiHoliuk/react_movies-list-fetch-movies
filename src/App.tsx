c0e0cf62091bc91590f9f2d6ec068d7aeff9970e
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-01-29 19:14:43 +0200

    movies-list-fetch-movies

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddMovie = (newMovie: Movie) => {
+    setMovies(prevMovies => {
+      if (prevMovies.some(movie => movie.imdbId === newMovie.imdbId)) {
+        return prevMovies;
+      }
+
+      return [...prevMovies, newMovie];
+    });
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAdd={handleAddMovie} />
       </div>
     </div>
   );
