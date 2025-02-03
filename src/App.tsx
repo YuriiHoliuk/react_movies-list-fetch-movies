c8038fafd49072c109f89a48337a6ba14409625b
misha-vynnyk <vinnikmisha94@gmail.com>
2025-02-03 10:44:13 +0100

    feat: Implement movie fetch and add to list functionality
    - Added API call to fetch movie details based on title.
    - Implemented logic for adding fetched movies to the list.
    - Updated state management for movie data.
    - Created functions to handle movie search and movie list addition.

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,63 @@ import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
+import { getMovie } from './api';
+import { movieDataToMovie } from './types/MovieDataToMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmitSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!searchTerm) {
+      return;
+    }
+
+    getMovie(searchTerm).then(innitMovie => {
+      if (!innitMovie || 'Error' in innitMovie) {
+        return;
+      }
+
+      setMovie(movieDataToMovie(innitMovie));
+    });
+  };
+
+  const handleSubmitAddToList = () => {
+    if (!movie) {
+      return;
+    }
+
+    setMovieList([...movieList, movie]);
+    setSearchTerm('');
+  };
+
+  // eslint-disable-next-line no-console
+  console.log('SearchTerm:', searchTerm);
+  // eslint-disable-next-line no-console
+  console.log('MovieList:', movieList);
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        <MoviesList movieList={movieList} />
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          movie={movie}
+          searchTerm={searchTerm}
+          handleSearchSet={handleSearchSet}
+          handleSubmit={handleSubmitSearch}
+          handleSubmitAddToList={handleSubmitAddToList}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './FindMovie.scss';
 
-export const FindMovie: React.FC = () => {
+interface Props {
+  movie: Movie | null;
+  searchTerm: string;
+  handleSearchSet: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: React.FormEvent) => void;
+  handleSubmitAddToList: () => void;
+}
+
+export const FindMovie: React.FC<Props> = ({
+  movie,
+  searchTerm,
+  handleSearchSet,
+  handleSubmit,
+  handleSubmitAddToList,
+}) => {
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSubmit}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -12,6 +27,8 @@ export const FindMovie: React.FC = () => {
 
           <div className="control">
             <input
+              onChange={handleSearchSet}
+              value={searchTerm}
               data-cy="titleField"
               type="text"
               id="movie-title"
@@ -36,22 +53,31 @@ export const FindMovie: React.FC = () => {
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {searchTerm ? (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleSubmitAddToList}
+              >
+                Add to the list
+              </button>
+            </div>
+          ) : (
+            ''
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie ? (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      ) : (
+        <p>No movie found</p>
+      )}
     </>
   );
 };
